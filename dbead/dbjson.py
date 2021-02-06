045560ad7250be640b69d2fd7ad3aab51d1fef69
exodojaffar <exodojaffar@gmail.com>
2021-02-05 20:25:09 -0400

    update: implemention of functions
    - select_courses
    - select_tasks
    - add_course
    - add_task

--- dbead/dbjson.py
+++ dbead/dbjson.py
@@ -6,7 +6,8 @@ from json import loads, dumps
 class DbEadJson(DbEadInterface):
 	"""docstring for DbJson"""
 	def __init__(self, path="", file="db.json"):
-		
+		self.__db_instance = None
+
 		self.path = path
 		self.file = file
 
@@ -14,16 +15,37 @@ class DbEadJson(DbEadInterface):
 		pass
 
 	def add_course(self, data) -> None:
+		if type(data) == type([]):
+			self.__db_instance['courses_table'] += data
+		else:
+			self.__db_instance['courses_table'].append(data)
 		pass
 
 	def add_task(self, data) -> None:
+		if type(data) == type([]):
+			self.__db_instance['tasks_table'] += data
+		else:
+			self.__db_instance['tasks_table'].append(data)
 		pass
 	
-	def select_courses(self) -> list:
-		pass
+	def select_courses(self, field: str, data) -> list:
+
+		def filter_courses(course):
+			if course[field] == data:
+				return course
+
+		courses_selected = list(map(self.__db_instance['courses_table'], filter_courses))
+
+		return courses_selected
 	
-	def select_tasks(self) -> list:
-		pass
+	def select_tasks(self, field, data) -> list:
+		def filter_tasks(task):
+			if task[field] == data:
+				return task
+
+		tasks_selected = list(map(self.__db_instance['tasks_table'], filter_tasks))
+
+		return tasks_selected
 	
 	def save_changes(self) -> None:
 		path = f'{self.path}{self.file}'
