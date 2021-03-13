7fd550be924d83ad00b109e410ea58956d375b8c
exodojaffar <exodojaffar@gmail.com>
2021-03-13 13:19:49 -0400

    feat: interface for Google Tasks Plataform

---
+++ tasksplataform/googletasks.py
@@ -0,0 +1,63 @@
+from tasksplataform.tasksinterface import TasksInterface
+from tasksplataform.googleapi import GoogleApi
+
+class GoogleTask(TasksInterface, GoogleApi):
+	"""docstring for GoogleTask"""
+	def __init__(self, scopes="https://www.googleapis.com/auth/tasks"):
+		service = 'tasks'
+		version = 'v1'
+
+		super(GoogleTask, self).__init__(scopes, service, version)
+
+		self.__lists = None
+
+		self.__tasklists = self._google_service.tasklists()
+		self.__tasks = self._google_service.tasks()
+
+		self.__update_list()
+
+		pass
+
+	def create_list(self, name) -> str:
+		result = self.__tasklists.insert(body={'title': name}).execute()
+		id_list = result['id']
+
+		return id_list
+	
+	def __update_list(self) -> None:	
+		self.__lists = self.tasklists.list().execute()['items']:
+		pass
+
+	def get_lists(self) -> list:
+		self.__update_list()
+		
+		return self.__lists
+	
+	def get_lists_name(self) -> list:
+		self.__update_list()
+		
+		lists_name = []
+
+		for list_item in self.__lists:
+			list_name = list_item['title']
+
+			lists_name.append(lists_name)
+
+			pass
+
+		return lists_name
+
+	def search_list(self, name) -> str:
+		id_list = ''
+
+		for list_item in self.__lists:
+			if list_item['title'] == name:
+				id_list = list_item['id']
+
+			continue
+
+		return id_list
+
+	def add_task_to_list(self, id_list, data) -> None:
+		self.__tasks.insert(tasklist=id_list, body=data).execute() 
+		pass
